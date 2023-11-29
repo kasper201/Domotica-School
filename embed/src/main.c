65ad81e04f152c7d559f1d4ffa77af5965a40d26
kasper201 <kasperjnssen@gmail.com>
2023-11-30 00:08:12 +0100

    Starting to understand now :D

--- embed/src/main.c
+++ embed/src/main.c
@@ -1,20 +1,34 @@
-#include <zephyr.h>
-#include <device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
 
-void main(void)
+static struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
+static struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {0});
+
+void led1And2()
 {
-    struct device *led_dev;
-
-    led_dev = device_get_binding("GPIO_0");
-    if (led_dev == NULL) {
-        printk("Error: didn't find GPIO_0 device\n");
+    k_msleep(1000);
+    printk("booting\n");
+    if (!gpio_is_ready_dt(&led0))
+    {
+        printk("Error: LED device %s is not ready\n", led0.port->name);
         return;
     }
 
-    while (1) {
-        gpio_pin_set(led_dev, 13, 1);
-        k_msleep(1000);
-        gpio_pin_set(led_dev, 13, 0);
-        k_msleep(1000);
-    }
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT);
+    gpio_pin_configure_dt(&led1, GPIO_OUTPUT);
+
+    gpio_pin_set_dt(&led0, 1);
+    gpio_pin_set_dt(&led1, 1);
+    k_msleep(1000);
+    int led_state = gpio_pin_get_dt(&led0);
+    printk("LED0 state: %d\n", led_state);
+}
+
+void main(void)
+{
+    led1And2();
 }
