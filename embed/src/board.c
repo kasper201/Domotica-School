44643c5d93f1b97f3c967d53576cdd2d9824e7e1
kasper201 <kasperjnssen@gmail.com>
2023-11-30 23:11:43 +0100

    EINDELIJK, wel wat code ehm geleend voor n onbepaalde tijd, t mag tho want t is apache 2.0

--- embed/src/board.c
+++ embed/src/board.c
@@ -1,3 +1,7 @@
+/*
+ * Button code borrowed from samples\basic\button
+ */
+
 //#include <zephyr/bluetooth/mesh.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -8,10 +12,12 @@
 
 #include "board.h"
 
-#define btn DT_N_S_buttons_S_button_0_ORD
-
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(btn, gpios,
-                                                              {0});
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
 static struct gpio_callback button_cb_data;
 
 
@@ -36,43 +42,39 @@ void ledInit() // most can be removed after testing
 
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
-                    uint32_t pins)
+		    uint32_t pins)
 {
-    printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-    // Add any additional logic you need when the button is pressed
-    // For example, toggle the LED state
-    bool current_led_state = gpio_pin_get_dt(&led);
-    gpio_pin_set_dt(&led, !current_led_state);
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 }
 
 void buttonInit(void)
 {
     int ret;
 
-    // Note: Use 'button' directly, not the address (&button)
-    if (!gpio_is_ready_dt(&button)) {
-        printk("Error: button device %s is not ready\n", button.port->name);
-        return;
-    }
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return;
+	}
 
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (ret != 0) {
-        printk("Error %d: failed to configure %s pin %d\n",
-               ret, button.port->name, button.pin);
-        return;
-    }
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return;
+	}
 
-    ret = gpio_pin_interrupt_configure_dt(&button,
-                                          GPIO_INT_EDGE_TO_ACTIVE);
-    if (ret != 0) {
-        printk("Error %d: failed to configure interrupt on %s pin %d\n",
-               ret, button.port->name, button.pin);
-        return;
-    }
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return;
+	}
 
-    gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-    gpio_add_callback(button.port, &button_cb_data);
-    printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 }
 
 void init()
