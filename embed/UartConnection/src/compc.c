fcc2bfd22143f6dbe46e6e5c38ca2494ebe60ed3
PineAppleOnTV <144004984+PineAppleOnTV@users.noreply.github.com>
2023-12-01 03:17:23 +0100

    Check update

--- embed/UartConnection/src/compc.c
+++ embed/UartConnection/src/compc.c
@@ -10,12 +10,14 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MESSAGE_SIZE 128 //Defines the maximum size for the incoming string
+#define MESSAGE_SIZE 200 //Defines the maximum size for the incoming string
 
 //Reads input from the application and decides what to do with it
 void readPc(char (*nodes)[MAX_NODE_INFO_STRING_LENGTH], bool *ledState)
 {
     char Message[MESSAGE_SIZE];
+    char first[MESSAGE_SIZE];
+    char second[MESSAGE_SIZE];
     k_msgq_get(&uart_msgq, &Message, K_NO_WAIT);
     k_msgq_cleanup(&uart_msgq);
 
@@ -33,13 +35,14 @@ void readPc(char (*nodes)[MAX_NODE_INFO_STRING_LENGTH], bool *ledState)
 
     if (strstr(Message, "UpdateActuator"))
     {
-        printk("check1\n");
+        extracStrings(Message, *first, *second);
+        printk("Nieuwschierig %s %s \n", first, second);
         char *token = strtok(Message, " ");
         token = strtok(NULL, " ");
         if(strstr(token, "STM32"))
         {
-            printk("check2\n");
             token = strtok(NULL, " ");
+            printk("check2 %s\n", token);
             if(strstr(token, "STM_LED"))
             {
                 printk("check3\n");
@@ -63,4 +66,25 @@ void readPc(char (*nodes)[MAX_NODE_INFO_STRING_LENGTH], bool *ledState)
         Message[i] = ' ';
     }
     k_msgq_cleanup(&uart_msgq);
+}
+
+//Gets word before first white space
+void extractStrings(const char *input, char *first, char *second) {
+    char *delimiter = ' ';
+    char *token = strtok((char *)input, delimiter);
+
+    if (token != NULL) {
+        // Copy the first part to the 'first' string
+        strncpy(first, token, strlen(token));
+        first[strlen(token)] = '\0';
+
+        // Get the next token (if any)
+        token = strtok(NULL, delimiter);
+
+        if (token != NULL) {
+            // Copy the second part to the 'second' string
+            strncpy(second, token, strlen(token));
+            second[strlen(token)] = '\0';
+        }
+    }
 }
