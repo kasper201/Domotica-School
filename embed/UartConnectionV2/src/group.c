2ee79dafc7b74ed79699d61a063e1ea55b99e709
kasper201 <kasperjnssen@gmail.com>
2024-01-16 01:01:09 +0100

    gn

--- embed/UartConnectionV2/src/group.c
+++ embed/UartConnectionV2/src/group.c
@@ -19,8 +19,7 @@ void unsubscribeFromGroup(uint16_t groupAddress, uint16_t elementAddress, uint16
     int err = 0;
     uint8_t status = 0;
     // Using the Bluetooth Mesh Configuration Client API to add a subscription
-    //err = bt_mesh_cfg_cli_mod_sub_add(netKeyIndex, address, elementAddress, groupAddress, mod_id, &status);
-    err = bt_mesh_cfg_cli_mod_sub_add(0x0000, 0x0002, 0x0002, 0xC000, 0x1000, &status);
+    err = bt_mesh_cfg_cli_mod_sub_del(netKeyIndex, address, elementAddress, groupAddress, mod_id, &status);
     if (err) {
         printk("Unsubscription failed (err %d)\n", err);
     }
